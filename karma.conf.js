b610f28351ebe78a7c95c3eb12ccb2eaaac77df5
Ziirf <49737643+Ziirf@users.noreply.github.com>
2020-11-02 13:42:22 +0100

    Name Changes

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/LEDProject'),
+      dir: require('path').join(__dirname, './coverage/Saido'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LEDProject'`, () => {
+  it(`should have as title 'Saido'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LEDProject');
+    expect(app.title).toEqual('Saido');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('LEDProject app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Saido app is running!');
   });
 });
