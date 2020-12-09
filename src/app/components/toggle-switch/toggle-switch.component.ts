977582a9df69dcbdb88c127ea552bfb26297aaa7
Nicolai Friis <nfriis24@gmail.com>
2020-12-09 13:23:47 +0100

    Commit Wednesday
    Toggle btn added

--- src/app/components/toggle-switch/toggle-switch.component.ts
+++ src/app/components/toggle-switch/toggle-switch.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-toggle-switch',
   templateUrl: './toggle-switch.component.html',
   styleUrls: ['./toggle-switch.component.scss']
 })
-export class ToggleSwitchComponent implements OnInit {
+export class ToggleSwitchComponent {
 
-  constructor() { }
+  @Input("bgColor")
+  bgColor: string = "orange";
 
-  ngOnInit(): void {
+  @Input("color")
+  color: string = "white";
+
+  @Output("value")
+  onSliderChange: EventEmitter<boolean> = new EventEmitter;
+
+  value: boolean = false;
+
+  constructor() { 
+    this.onSliderChange.emit(this.value);
   }
 
+  sliderChange() {
+    this.value = !this.value;
+    this.onSliderChange.emit(this.value);
+  }
 }
